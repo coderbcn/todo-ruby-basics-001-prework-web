67830691de575cfda0106188771985090cf5033a
coderbcn <suman.gandham@zapper.com>
2015-09-20 10:46:54 +0200

    I really have to try hard

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,39 @@
 def division(num1, num2)
+ num1 = 42
+ num2 = 7
+ div = num1/num2
+ div
+end
+
+def assign_variable(name)
+  name
+end
+
+def argue(phrase)
+  
+  phrase
+  
+  end
+
+def greeting(greet,name)
+  greet+name
+
+end
+
+def return_a_value(value="Nice")
+  value
+ end
  
-end
-
-def assign_variable(value)
-  
-end
-
-def argue
-  
-end
-
-def greeting
-
-end
-
-def return_a_value
-
-end
-
 def last_evaluated_value
 
 end
 
-def pizza_party
+def last_evaluated_value(expert="expert")
+  expert
+
+end
+
+def pizza_party(topping="cheese")
+  topping
 
 end
